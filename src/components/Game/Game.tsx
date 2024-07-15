1f064b4730f7e75b4972baeb909b637caed6643a
gabriel.vannier <vannier.gabriel@gmail.com>
2024-07-15 14:44:01 +0200

    move possibleguess to game component

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,12 +1,41 @@
 import { on } from "events";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import GuessList from "./GuessList";
 import Input from "./Input";
-
+import * as Papa from "papaparse";
+import { Person } from "./types";
 
 function Game() {
-    const [guesses, setGuesses] = React.useState<string[]>([]);
+    const [guesses, setGuesses] = useState<string[]>([]);
+    const [possibleGuessesRecord, setPossibleGuessesRecord] = useState<Record<string, Person>>({});
+    const [isLoading, setIsLoading] = useState(true);
 
+    useEffect(() => {
+        load();
+    }, []);
+
+    const load = function () {
+        fetch('./data.csv')
+            .then(response => response.text())
+            .then(responseText => {
+                var data = Papa.parse(responseText);
+                if (typeof (data.data) === "object") {
+                    const newPossibleGuessesRecord: Record<string, Person> = {};
+                    data.data.forEach(line => {
+                        if (Array.isArray(line) && line.length === 6) {
+                            newPossibleGuessesRecord[line[0]] = { name: line[0], birthDate: line[1], deathDate: line[2], party: line[3], highestRole: line[4], sexe: line[5] };
+                        }
+                    });
+                    setPossibleGuessesRecord(newPossibleGuessesRecord);
+                    setIsLoading(false);
+                }
+            })
+            .catch(error => {
+                console.error("Error loading data:", error);
+                setIsLoading(false);
+            });
+    };
+    
     const handleSubmit = (e: React.FormEvent<Element>) => {
         e.preventDefault();
         const guessInput = document.getElementById("guess-input") as HTMLInputElement;
@@ -15,10 +44,13 @@ function Game() {
         setGuesses(prevGuesses => [...prevGuesses, newGuess]);
         guessInput.value = "";
     };
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div className="Game">
-            <Input handleSubmit={handleSubmit} />
+            <Input handleSubmit={handleSubmit} possibleGuessesRecord={possibleGuessesRecord} />
             <GuessList guesses={guesses} />
         </div>
     );
--- src/components/Game/Input.tsx
+++ src/components/Game/Input.tsx
@@ -1,47 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Autocomplete, TextField } from "@mui/material";
-import * as Papa from "papaparse";
 import { Person } from "./types";
 
 
 type InputProps = {
     handleSubmit: (e: React.FormEvent<Element>) => void;
+    possibleGuessesRecord: Record<string, Person>;
 };
-
-function Input({ handleSubmit }: InputProps) {
-    const [possibleGuessesRecord, setPossibleGuessesRecord] = useState<Record<string, Person>>({});
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        load();
-    }, []);
-
-    const load = function () {
-        fetch('./data.csv')
-            .then(response => response.text())
-            .then(responseText => {
-                var data = Papa.parse(responseText);
-                if (typeof (data.data) === "object") {
-                    const newPossibleGuessesRecord: Record<string, Person> = {};
-                    data.data.forEach(line => {
-                        if (Array.isArray(line) && line.length === 6) {
-                            newPossibleGuessesRecord[line[0]] = { name: line[0], birthDate: line[1], deathDate: line[2], party: line[3], highestRole: line[4], sexe: line[5] };
-                        }
-                    });
-                    setPossibleGuessesRecord(newPossibleGuessesRecord);
-                    setIsLoading(false);
-                }
-            })
-            .catch(error => {
-                console.error("Error loading data:", error);
-                setIsLoading(false);
-            });
-    };
-
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
+function Input(
+    { handleSubmit, possibleGuessesRecord }: InputProps
+){
     return (
         <div className="Input-guess">
             <form onSubmit={handleSubmit}>
@@ -49,7 +17,7 @@ function Input({ handleSubmit }: InputProps) {
                     id="guess-input"
                     options={Object.values(possibleGuessesRecord)}
                     getOptionLabel={(option) => option.name}
-                    renderInput={(params) => <TextField {...params} label="Personality" />}
+                    renderInput={(params) => <TextField {...params} label="Personnalité politique" />}
                 />
             </form>
         </div>
