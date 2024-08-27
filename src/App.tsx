47932704deee378755530dcf947294c5fab82a79
gabriel.vannier <vannier.gabriel@gmail.com>
2024-08-27 20:06:14 +0200

    add Analytics and speedinsight from vercel

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Game from './components/Game/Game';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
+
 
 function App() {
   return (
     <div className="App">
+      <Analytics/>
+      <SpeedInsights/>
       <Header />
       <Game />
     </div>
